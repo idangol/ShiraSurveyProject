8570b7bfafdd34638afd01cd9c388c14a43b3995
Idan <goldman.idan@gmail.com>
2022-06-04 23:19:29 -0400

    option to show values on bars in plots

--- publisher.py
+++ publisher.py
@@ -33,7 +33,7 @@ class Publisher:
         plt.show() # adjust the plot visualization and save!
 
 
-    def publish_black_n_white(self, color):
+    def publish_black_n_white(self, color, add_bar_labels):
 
         # Plot configs:
         plt.figure(figsize=(12, 5))
@@ -47,13 +47,22 @@ class Publisher:
             first_column_as_list = self.data.iloc[:, 0].tolist()
             second_column_as_list = self.data.iloc[:, 1].tolist()
             y_tic_locations = np.arange(len(first_column_as_list))
-            bars1 = plt.barh(y_tic_locations + 0.2, first_column_as_list, height=0.4, color=color[0])
-            bars2 = plt.barh(y_tic_locations - 0.2, second_column_as_list, height=0.4, color=color[1])
+            bars1 = plt.barh(y_tic_locations + 0.2, first_column_as_list, height=0.4, color=color[0], edgecolor='black')
+            bars2 = plt.barh(y_tic_locations - 0.2, second_column_as_list, height=0.4, color=color[1], edgecolor='black')
             plt.legend([bars1, bars2], [self.data.columns.values.tolist()[0], self.data.columns.values.tolist()[1]])
             plt.yticks(ticks=y_tic_locations, labels=index)
 
+            if add_bar_labels:
+                for i, v in enumerate(first_column_as_list):
+                    plt.text(v + 0.5, i + 0.1, str(int(v)) + "%", color='black', fontsize='x-small' )
+                for i, v in enumerate(second_column_as_list):
+                    plt.text(v + 0.5, i - 0.3, str(int(v)) + "%", color='black', fontsize='x-small' )
+
+
         else:
             plt.barh(index, self.data[0], color=color)
+            for i, v in enumerate(self.data[0]):
+                plt.text(v + 0.25, i - 0.15, str(int(v)) + "%", color='black')
 
         plt.xlabel(self.XLABEL)
         plt.xticks(ticks=[25, 50, 75, 100], labels=["25%", "50%", "75%", "100%"])
